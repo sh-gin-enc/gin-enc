c1b8adb0dc4bcbf6d7dcf5101194b995a58ad33f
wulei <27409165@qq.com>
2025-04-02 00:54:21 +0000

    new file

---
+++ enc.go
@@ -0,0 +1,12 @@
+// mathutils.go
+package mylibrary
+
+// Add adds two integers and returns the result.
+func Add(a int, b int) int {
+    return a + b
+}
+
+// Subtract subtracts the second integer from the first and returns the result.
+func Subtract(a int, b int) int {
+    return a - b
+}
